b0cf533977a17c716e70eed4ca8951d76ab3a6d4
Georges Toth <georges@trypill.org>
2010-10-10 17:43:33 +0200

    initial pylons project checkin

---
+++ MeMaTool/mematool/websetup.py
@@ -0,0 +1,18 @@
+"""Setup the MeMaTool application"""
+import logging
+
+import pylons.test
+
+from mematool.config.environment import load_environment
+from mematool.model.meta import Session, metadata
+
+log = logging.getLogger(__name__)
+
+def setup_app(command, conf, vars):
+    """Place any commands to setup mematool here"""
+    # Don't reload the app if it was loaded under the testing environment
+    if not pylons.test.pylonsapp:
+        load_environment(conf.global_conf, conf.local_conf)
+
+    # Create the tables if they don't already exist
+    metadata.create_all(bind=Session.bind)
