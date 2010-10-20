9793ac995f9cd47dab5fe852dde97016da63cd06
Georges Toth <georges@trypill.org>
2010-10-21 00:05:27 +0200

    Add copyright notice

--- MeMaTool/mematool/controllers/members.py
+++ MeMaTool/mematool/controllers/members.py
@@ -1,3 +1,24 @@
+#
+#    MeMaTool (c) 2010 Georges Toth <georges _at_ trypill _dot_ org>
+#
+#
+#    This file is part of MeMaTool.
+#
+#
+#    MeMaTool is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with MeMaTool.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import logging
 
 from pylons import request, response, session, tmpl_context as c, url
