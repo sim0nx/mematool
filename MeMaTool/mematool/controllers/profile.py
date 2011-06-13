8cb61c0f0ea1b68c631029f3725b05cc3fa25b55
Georges Toth <georges@trypill.org>
2011-06-13 23:17:09 +0200

    user modifiable profile page

---
+++ MeMaTool/mematool/controllers/profile.py
@@ -0,0 +1,206 @@
+#
+#	MeMaTool (c) 2010 Georges Toth <georges _at_ trypill _dot_ org>
+#
+#
+#	This file is part of MeMaTool.
+#
+#
+#	MeMaTool is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	Foobar is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with MeMaTool.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import logging
+
+from pylons import request, response, session, tmpl_context as c, url
+from pylons.controllers.util import abort, redirect
+from pylons import config
+
+from mematool.lib.base import BaseController, render, Session
+from mematool.model import Member, TmpMember
+
+log = logging.getLogger(__name__)
+
+from mematool.lib.syn2cat.ldapConnector import LdapConnector
+import re
+from mematool.lib.syn2cat import regex
+
+from webob.exc import HTTPUnauthorized
+
+import gettext
+_ = gettext.gettext
+
+
+
+class ProfileController(BaseController):
+
+	def __init__(self):
+		super(ProfileController, self).__init__()
+		self.ldapcon = LdapConnector()
+
+
+	def __before__(self):
+		super(ProfileController, self).__before__()
+
+		if not self.identity:
+			redirect(url(controller='error', action='unauthorized'))
+
+
+	def _require_auth(self):
+		return True
+
+
+	def index(self):
+		return self.edit()
+
+
+	def edit(self):
+		c.heading = 'Edit profile'
+		c.formDisabled = ''
+
+		try:
+			member = Member( session['identity'] )
+
+			if member.validate:
+				tm = Session.query(TmpMember).filter(TmpMember.id == member.uidNumber).first()
+				member.cn = tm.gn + ' ' + tm.sn
+				member.gn = tm.gn
+				member.sn = tm.sn
+				member.birthDate = tm.birthDate
+				member.homePostalAddress = tm.homePostalAddress
+				member.phone = tm.phone
+				member.mobile = tm.mobile
+				member.mail = tm.mail
+
+				c.formDisabled = 'disabled'
+
+			c.member = member
+
+			return render('/profile/edit.mako')
+
+		except LookupError:
+			print 'Edit :: No such ldap user !'
+
+
+		return 'ERROR 4x0'
+
+
+	def checkMember(f):
+		def new_f(self):
+			# @TODO request.params may contain multiple values per key... test & fix
+			formok = True
+			errors = []
+
+			if not 'sn' in request.params or request.params['sn'] == '' or len(request.params['sn']) > 20:
+				formok = False
+				errors.append(_('Invalid surname'))
+
+			if not 'gn' in request.params or request.params['gn'] == '' or len(request.params['gn']) > 20:
+				formok = False
+				errors.append(_('Invalid given name'))
+
+			if not 'birthDate' in request.params or not re.match(regex.date, request.params['birthDate'], re.IGNORECASE):
+				formok = False
+				errors.append(_('Invalid birth date'))
+
+			if not 'homePostalAddress' in request.params or request.params['homePostalAddress'] == '' or len(request.params['homePostalAddress']) > 100:
+				formok = False
+				errors.append(_('Invalid address'))
+
+			#if 'phone' in request.params and request.params['phone'] != '' and not re.match(regex.phone, request.params['phone'], re.IGNORECASE):
+			if 'phone' in request.params and request.params['phone'] != '' and not self._isParamStr('phone', max_len=30, regex=regex.phone):
+				formok = False
+				errors.append(_('Invalid phone number'))
+
+			if not 'mobile' in request.params or not re.match(regex.phone, request.params['mobile'], re.IGNORECASE):
+				formok = False
+				errors.append(_('Invalid mobile number'))
+
+			if not 'mail' in request.params or not re.match(regex.email, request.params['mail'], re.IGNORECASE):
+				formok = False
+				errors.append(_('Invalid e-mail address'))
+
+			if self._isParamStr('userPassword') and self._isParamStr('userPassword2'):
+				if request.params['userPassword'] != request.params['userPassword2']:
+					formok = False
+					errors.append(_('Passwords don\'t match'))
+				elif len(request.params['userPassword']) < 8:
+					formok = False
+					errors.append(_('Password too short'))
+
+			if not formok:
+				session['errors'] = errors
+				session['reqparams'] = {}
+
+				# @TODO request.params may contain multiple values per key... test & fix
+				for k in request.params.iterkeys():
+					if (k == 'full_member' or k == 'locked_member')  and request.params[k] == 'on':
+						session['reqparams'][k] = 'checked'
+					else:
+						session['reqparams'][k] = request.params[k]
+				
+				session.save()
+
+				redirect(url(controller='profile', action='edit'))
+
+
+			return f(self)
+		return new_f
+
+
+	@checkMember
+	def doEdit(self):
+		m = Member(session['identity'])
+
+		if m.validate:
+			# member locked for validation
+			redirect(url(controller='error', action='unauthorized'))
+		else:
+			changes = False
+
+			if request.params['sn'] != m.sn or\
+				request.params['gn'] != m.gn or\
+				request.params['birthDate'] != m.birthDate or\
+				request.params['homePostalAddress'] != m.homePostalAddress or\
+				request.params['phone'] != m.phone or\
+				request.params['mobile'] != m.mobile or\
+				request.params['mail'] != m.mail:
+				changes = True
+
+			if changes:
+				tm = TmpMember(m.uidNumber)
+				tm.sn = request.params['sn']
+				tm.gn = request.params['gn']
+				tm.birthDate = request.params['birthDate']
+				tm.homePostalAddress = request.params['homePostalAddress']
+
+				if 'phone' not in request.params or request.params['phone'] == '':
+					tm.phone = '>>REMOVE<<'
+				else:
+					tm.phone = request.params['phone']
+
+				tm.mobile = request.params['mobile']
+				tm.mail = request.params['mail']
+
+				Session.add(tm)
+				Session.commit()
+
+				session['flash'] = 'Changes saved!'
+				session.save()
+
+			if 'userPassword' in request.params and request.params['userPassword'] != '':
+				m.setPassword(request.params['userPassword'])
+				m.save()
+	                        session['flash'] = 'Password updated!'
+	                        session.save()
+
+		redirect(url(controller='profile', action='index'))
---
+++ MeMaTool/mematool/templates/profile/edit.mako
@@ -0,0 +1,199 @@
+<%inherit file="/base.mako" />
+
+<%!
+def getFormVar(s, c, var):
+	if 'reqparams' in s:
+		if var in s['reqparams']:
+			return s['reqparams'][var]
+
+	if hasattr(c, 'member'):
+		if var in vars(c.member):
+			return vars(c.member)[var]
+
+	if var is 'gidNumber':
+		return 100
+	elif var is 'loginShell':
+		return '/bin/false'
+	elif var is 'homeDirectory':
+		return '/home/'
+
+	return ''
+%>
+
+<form method="post" action="${url(controller='profile', action='doEdit')}" name="recordform">
+
+<table class="table_content" width="95%%">
+	% if 'errors' in session:
+	% if len(session['errors']) > 0:
+	<tr>
+		<td>&nbsp;</td>
+		<td>
+		% for k in session['errors']:
+		<font color="red">${k}</font><br>
+		% endfor
+		</td>
+	</tr>
+		<%
+		del session['errors']
+		session.save()
+		%>
+	% else:
+		<%
+                del session['errors']
+                session.save()
+                %>
+	% endif
+	% endif
+        <tr>
+                <td class="table_title">
+                        ${_('Username')}
+                </td>
+		<td>
+			${c.member.uid}
+		</td>
+	</tr>
+        <tr>
+                <td class="table_title">
+                        ${_('User ID')}
+                </td>
+                <td>
+                        ${c.member.uidNumber}
+                </td>
+        </tr>
+        <tr>
+                <td class="table_title">
+                        ${_('Additional groups')}
+                </td>
+                <td>
+                        ${_('full member')} ${getFormVar(session, c, 'full_member')}
+                        ${_('locked member')} ${getFormVar(session, c, 'locked_member')}
+                </td>
+        </tr>
+	<tr>
+                <td class="table_title">
+                        ${_('Common name')}
+                </td>
+                <td>
+                        ${getFormVar(session, c, 'cn')}
+                </td>
+	</tr>
+	<tr>
+                <td class="table_title">
+                        ${_('Surname')}
+                </td>
+                <td>
+                        <input type="text" name="sn" value="${getFormVar(session, c, 'sn')}" class="input" ${c.formDisabled}>
+                </td>
+        </tr>
+	<tr>
+                <td class="table_title">
+                        ${_('Given name')}
+                </td>
+                <td>
+                        <input type="text" name="gn" value="${getFormVar(session, c, 'gn')}" class="input" ${c.formDisabled}>
+                </td>
+        </tr>
+        <tr>
+                <td class="table_title">
+                        ${_('Birth Date')} (YYYY-MM-DD)
+                </td>
+                <td>
+                        <input type="text" name="birthDate" value="${getFormVar(session, c, 'birthDate')}" class="input" ${c.formDisabled}>
+                </td>
+        </tr>
+        <tr>
+                <td class="table_title">
+                        ${_('Address')}
+                </td>
+                <td>
+                        <textarea rows='10' cols='60' name="homePostalAddress" ${c.formDisabled}>${getFormVar(session, c, 'homePostalAddress')}</textarea>
+                </td>
+        </tr>
+        <tr>
+                <td class="table_title">
+                        ${_('Phone')} (+xxx.yyyyyyyyy)
+                </td>
+                <td>
+                        <input type="text" name="phone" value="${getFormVar(session, c, 'phone')}" class="input" ${c.formDisabled}>
+                </td>
+        </tr>
+        <tr>
+                <td class="table_title">
+                        ${_('Mobile')} (+xxx.yyyyyyyyy)
+                </td>
+                <td>
+                        <input type="text" name="mobile" value="${getFormVar(session, c, 'mobile')}" class="input" ${c.formDisabled}>
+                </td>
+        </tr>
+        <tr>
+                <td class="table_title">
+                        ${_('E-Mail')}
+                </td>
+                <td>
+                        <input type="text" name="mail" value="${getFormVar(session, c, 'mail')}" class="input" ${c.formDisabled}>
+                </td>
+        </tr>
+        <tr>
+                <td class="table_title">
+                        ${_('Login Shell')}
+                </td>
+                <td>
+                        ${getFormVar(session, c, 'loginShell')}
+                </td>
+        </tr>
+	<tr>
+                <td class="table_title">
+                        ${_('Home directory')}
+                </td>
+                <td>
+                        ${getFormVar(session, c, 'homeDirectory')}
+                </td>
+        </tr>
+        <tr>
+                <td class="table_title">
+                        ${_('Member since')} (YYYY-MM-DD)
+                </td>
+                <td>
+                        ${getFormVar(session, c, 'arrivalDate')}
+                </td>
+        </tr>
+        <tr>
+                <td class="table_title">
+                        ${_('SSH Public Key')}
+                </td>
+                <td>
+                        <textarea rows='10' cols='60' disabled name="sshPublicKey">${getFormVar(session, c, 'sshPublicKey')}</textarea>
+                </td>
+        </tr>
+        <tr>
+                <td class="table_title">
+                        ${_('Password')} (min 8)
+                </td>
+                <td>
+                        <input type="password" name="userPassword" value="" class="input" ${c.formDisabled}>
+                </td>
+        </tr>
+        <tr>
+                <td class="table_title">
+                        ${_('Repeat Password')} (min 8)
+                </td>
+                <td>
+                        <input type="password" name="userPassword2" value="" class="input" ${c.formDisabled}>
+                </td>
+        </tr>
+	<tr>
+		<td>
+			&nbsp;
+		</td>
+                <td>
+                        <input type="submit" name="" value="Submit" class="input" ${c.formDisabled}>
+                </td>
+        </tr>
+</table>
+</form>
+
+<%
+if 'reqparams' in session:
+	del session['reqparams']
+	session.save()
+%>
