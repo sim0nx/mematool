efc9fe09ea9c62222f1d44d7406514d4d9de524d
Georges Toth <georges@trypill.org>
2011-06-13 23:16:39 +0200

    temporary member model; used for temp saving user modifications requiring validation

---
+++ MeMaTool/mematool/model/tmpmember.py
@@ -0,0 +1,52 @@
+#
+#    MeMaTool (c) 2010 Georges Toth <georges _at_ trypill _dot_ org>
+#
+#
+#    This file is part of MeMaTool.
+#
+#
+#    MeMaTool is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Foobar is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with MeMaTool.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from sqlalchemy import schema, types, orm, create_engine, Table, Column, Integer, String, MetaData, ForeignKey, Boolean, Date, ForeignKeyConstraint
+from sqlalchemy.ext.declarative import declarative_base
+from mematool.model.meta import Base
+from mematool.lib.base import Session
+
+
+class TmpMember(Base):
+	__tablename__ = 'tmpMember'
+	__table_args__ = (
+		{'mysql_engine':'InnoDB'}
+		)
+
+	id			= Column(Integer, primary_key=True)
+	gn			= Column(String(60))
+	sn 			= Column(String(60))
+	birthDate		= Column(String(10))
+	homePostalAddress	= Column(String(255))
+	phone			= Column(String(30))
+	mobile			= Column(String(30))
+	mail			= Column(String(255))
+
+	def __init__(self, uidNumber):
+		self.id = uidNumber
+
+	def __str__(self):
+		return "<TmpMember('id=%d, gn=%s', sn=%s, birthDate=%s, homePostalAddress=%s, phone=%s, mobile=%s, mail=%s)>" % (self.id, self.gn, self.sn, self.birthDate, self.homePostalAddress, self.phone, self.mobile, self.mail)
+
+
+	def save(self):
+		Session.commit()
+
