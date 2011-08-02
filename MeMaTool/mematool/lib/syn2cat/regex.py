ea29cc9f6e93b5d25cbedfca6c816b0df80d6b0d
Georges Toth <georges@trypill.org>
2011-08-02 17:39:10 +0200

    add support for rsa-dss keys

--- MeMaTool/mematool/lib/syn2cat/regex.py
+++ MeMaTool/mematool/lib/syn2cat/regex.py
@@ -6,5 +6,5 @@ date = r'^(19\d\d|2\d\d\d)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$'
 phone = r'^\+(\d){1,3}\.(\d){4,}$' # +123.1234   after the dot, minimum 4 numbers
 homeDirectory = r'^/home/[a-z0-9_]*$'
 loginShell = r'^/bin/[a-z0-9]*$'
-sshKey = r'^ssh-rsa AAAAB3NzaC1yc2EAAAA[A-Za-z0-9+/]{150,1200}'
+sshKey = r'^ssh-(?:rsa AAAAB3NzaC1yc2EAAAA|dss AAAB3NzaC1kc3MAAA)[A-Za-z0-9+/]{150,1200}'
 pgpKey = r'^([a-zA-Z0-9]{4}\s*){10}$'
