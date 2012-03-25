b0efce8c98e1d22d9e196f8dfbc8680485b66f0c
Georges Toth <georges@trypill.org>
2012-03-26 00:13:28 +0200

    add domain regex

--- MeMaTool/mematool/lib/syn2cat/regex.py
+++ MeMaTool/mematool/lib/syn2cat/regex.py
@@ -3,6 +3,7 @@
 
 username = r'^[a-zA-Z0-9_]{1,20}$'
 email = r'\b[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,4}\b'
+domain = r'^(?:[A-Z0-9-]+\.)+[A-Z]{2,4}$'
 date = r'^(19\d\d|2\d\d\d)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$'
 phone = r'^\+(\d){1,3}\.(\d){4,}$' # +123.1234   after the dot, minimum 4 numbers
 homeDirectory = r'^/home/[a-z0-9_]*$'
