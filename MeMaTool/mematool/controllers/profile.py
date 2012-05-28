61f27d336d6637a9a3927eca4fa7f25c10e72e4c
Georges Toth <georges@trypill.org>
2012-05-28 23:29:27 +0200

    add copyright

--- MeMaTool/mematool/controllers/profile.py
+++ MeMaTool/mematool/controllers/profile.py
@@ -1,23 +1,20 @@
 #
-# MeMaTool (c) 2010 Georges Toth <georges _at_ trypill _dot_ org>
-#
+# Copyright (c) 2012 Georges Toth <georges _at_ trypill _dot_ org>
 #
 # This file is part of MeMaTool.
 #
-#
 # MeMaTool is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Foobar is distributed in the hope that it will be useful,
+# MeMaTool is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Affero General Public License
 # along with MeMaTool.  If not, see <http://www.gnu.org/licenses/>.
-
 
 import logging
 
--- MeMaTool/mematool/model/alias.py
+++ MeMaTool/mematool/model/alias.py
@@ -1,22 +1,20 @@
 #
-#    MeMaTool (c) 2010 Georges Toth <georges _at_ trypill _dot_ org>
+# Copyright (c) 2012 Georges Toth <georges _at_ trypill _dot_ org>
 #
+# This file is part of MeMaTool.
 #
-#    This file is part of MeMaTool.
+# MeMaTool is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# MeMaTool is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 #
-#    MeMaTool is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    Foobar is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with MeMaTool.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with MeMaTool.  If not, see <http://www.gnu.org/licenses/>.
 
 
 class Alias(object):
