e281bdb65e29729a9a97f8587f3b130b9bb6acae
Georges Toth <georges@trypill.org>
2012-05-28 12:22:24 +0200

    replace basic input checks by lechecker checks

--- MeMaTool/mematool/controllers/auth.py
+++ MeMaTool/mematool/controllers/auth.py
@@ -3,11 +3,12 @@ import logging
 from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
 from pylons.i18n.translation import _
+from pylons.decorators.secure import https
 
 from mematool.lib.base import BaseController, render
-from pylons.decorators.secure import https
 from mematool.lib.syn2cat.auth.auth_ldap import LDAPAuthAdapter
 from mematool.model.ldapModelFactory import LdapModelFactory
+from mematool.model.lechecker import ParamChecker, InvalidParameterFormat
 from mematool.lib.syn2cat.crypto import encodeAES, decodeAES
 
 
@@ -27,48 +28,50 @@ class AuthController(BaseController):
   @https()
   def login(self):
     if self.identity is not None:
-      if 'came_from' in request.params:
-        #redirect(request.params['came_from'])
-        redirect(url(controller='members', action='showAllMembers'))
-      else:
-        redirect(url(controller='members', action='showAllMembers'))
+      redirect(url(controller='members', action='showAllMembers'))
 
     c.heading = 'MeMaTool'
 
     return render('/auth/login.mako')
 
   def doLogin(self):
-    if not 'username' in request.params or request.params['username'] == '' or not 'password' in request.params or request.params['password'] == '':
-      print "crap"
-    else:
-      authAdapter = LDAPAuthAdapter()
-      ret = authAdapter.authenticate(request.params['username'], request.params['password'])
+    try:
+      ParamChecker.checkUsername('username', param=True)
+      ParamChecker.checkPassword('password', 'password', param=True)
+    except InvalidParameterFormat as ipf:
+      session['flash'] = _('Login failed!')
+      session['flash_class'] = 'error'
+      session.save()
 
-      if ret:
-        self._clearSession()
+      redirect(url(controller='auth', action='login'))
+ 
+    authAdapter = LDAPAuthAdapter()
+    ret = authAdapter.authenticate(request.params['username'], request.params['password'])
 
-        session['identity'] = request.params['username']
-        session['secret'] = encodeAES(request.params['password'])
-        lmf = LdapModelFactory()
-        session['groups'] = lmf.getUserGroupList(request.params['username'])
-        # dummy call to set the variable
-        self.isFinanceAdmin()
-        session.save()
+    if ret:
+      self._clearSession()
 
-        log.info(request.params['username'] + ' logged in')
+      session['identity'] = request.params['username']
+      session['secret'] = encodeAES(request.params['password'])
+      lmf = LdapModelFactory()
+      session['groups'] = lmf.getUserGroupList(request.params['username'])
+      # dummy call to set the variable
+      self.isFinanceAdmin()
+      session.save()
 
-        if 'after_login' in session and not 'forbidden' in session["after_login"]:
-          print session["after_login"]
-          redirect(session["after_login"])
-        else:
-          if self.isAdmin():
-            redirect(url(controller='members', action='index'))
-          else:
-            redirect(url(controller='profile', action='index'))
+      log.info(request.params['username'] + ' logged in')
+
+      if 'after_login' in session and not 'forbidden' in session["after_login"]:
+        redirect(session["after_login"])
       else:
-        session['flash'] = _('Login failed!')
-        session['flash_class'] = 'error'
-        session.save()
+        if self.isAdmin():
+          redirect(url(controller='members', action='index'))
+        else:
+          redirect(url(controller='profile', action='index'))
+    else:
+      session['flash'] = _('Login failed!')
+      session['flash_class'] = 'error'
+      session.save()
 
     redirect(url(controller='auth', action='login'))
 
