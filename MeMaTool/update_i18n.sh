91cb13a5887e621040c153614f60f9d80328acaa
Georges Toth <georges@trypill.org>
2012-06-04 23:32:26 +0200

    use msgmerge as workaround for python babel as the latter one does not support "lb" O_o

--- MeMaTool/update_i18n.sh
+++ MeMaTool/update_i18n.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 python setup.py extract_messages
-python setup.py update_catalog
+#python setup.py update_catalog
 
+for k in `find mematool/i18n -name \*.po`
+do
+  echo $k
+  msgmerge $k mematool/i18n/mematool.pot > ${k}_
+  mv ${k}_ $k
+done
