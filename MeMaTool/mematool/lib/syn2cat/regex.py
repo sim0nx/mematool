9b4adc1f195e74283833260fafd124a0b66f98b9
Georges Toth <georges@trypill.org>
2012-12-27 19:19:32 +0100

    enforce all-lowercase usernames

--- MeMaTool/mematool/lib/syn2cat/regex.py
+++ MeMaTool/mematool/lib/syn2cat/regex.py
@@ -1,7 +1,7 @@
 ''' This file includes a bunch of commonly used regular expressions '''
 
 
-username = r'^[a-zA-Z0-9_]{1,20}$'
+username = r'^[a-z0-9_]{1,20}$'
 email = r'\b[A-Z0-9._%+-]+@(?:[A-Z0-9-]+\.)+[A-Z]{2,4}\b'
 domain = r'^(?:[A-Z0-9-]+\.)+[A-Z]{2,4}$'
 date = r'^(19\d\d|2\d\d\d)-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$'
