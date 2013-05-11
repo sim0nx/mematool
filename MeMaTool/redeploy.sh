2f1bba4d451df3b58b570d0d8518acee780ff144
Georges Toth <georges@trypill.org>
2013-05-11 19:24:54 +0200

    encoding issues

--- MeMaTool/redeploy.sh
+++ MeMaTool/redeploy.sh
@@ -1,6 +1,6 @@
 python setup.py egg_info
 python setup.py bdist_egg
 
-virtualenv/bin/easy_install -U dist/MeMaTool-0.1dev-py2.6.egg
+virtualenv/bin/easy_install -U dist/MeMaTool-0.1dev-py2.7.egg
 
-sudo /etc/init.d/uwsgi restart mematool
+sudo service uwsgi restart mematool
