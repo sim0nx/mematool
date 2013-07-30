d1057b1b3c5578f6279334e1de28e69e6040ffce
Georges Toth <georges.toth@govcert.etat.lu>
2013-07-30 22:39:37 +0200

    fix imports

--- MeMaTool/mematool/model/ldapmodel.py
+++ MeMaTool/mematool/model/ldapmodel.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with MeMaTool.  If not, see <http://www.gnu.org/licenses/>.
 
-
+from pylons.i18n.translation import _
 from mematool.lib.base import Session
 from mematool.model.dbmodel import TmpMember
 
